a3b70ba89d0aa39558cbb162de764124c6127443
Fabio Muratori <f.muratori@aivolutionsrl.com>
2022-10-30 17:31:01 +0100

    Added basic nodejs server

---
+++ server-frontend/src/index.ts
@@ -0,0 +1,35 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+
+// import path from 'path';
+// import express, { Express, Request, Response } from 'express';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const app: Express = express();
+// const port = process.env.PORT;
+
+// app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
+
+
+// app.get('/', function (req: Request, res:Response) {
+//   res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+// });
