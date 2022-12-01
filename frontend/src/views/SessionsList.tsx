4de950b272131d5269075c4f5e238bc133381122
Fabio Muratori <fabio.muratori2@studio.unibo.it>
2022-12-01 01:27:45 +0100

    Tested frontend-server subproject manual deployment with the updated React frontend subproject

--- frontend/src/views/SessionsList.tsx
+++ frontend/src/views/SessionsList.tsx
@@ -3,7 +3,12 @@ import './SessionsList.css';
 
 function SessionsList() {
   return (
-    <Link to={`/create-session`}>Your Name</Link>
+    <>
+      <Link to={`/create-session`}>Your Name</Link>
+      <Link to={`/session`}>Your Name</Link>
+      <Link to={`/game`}>Your Name</Link>
+      <Link to={`/create-session`}>Your Name</Link>
+    </>
   );
 }
 
--- server-frontend/src/index.ts
+++ server-frontend/src/index.ts
@@ -1,20 +1,3 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
-
-// import path from 'path';
 // import express, { Express, Request, Response } from 'express';
 // import dotenv from 'dotenv';
 
@@ -23,13 +6,30 @@ app.listen(port, () => {
 // const app: Express = express();
 // const port = process.env.PORT;
 
-// app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
-
-
-// app.get('/', function (req: Request, res:Response) {
-//   res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Express + TypeScript Server');
 // });
 
 // app.listen(port, () => {
 //   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 // });
+
+import path from 'path';
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
+
+
+app.get('/', function (req: Request, res:Response) {
+  res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
